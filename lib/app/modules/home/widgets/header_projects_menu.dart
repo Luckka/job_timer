b9d4511fe2ec7da0001220e8bf5d1d311ea8ab2e
Lucka <lucka.dev.pj@dippingcode.com.br>
2022-06-13 21:16:43 -0300

    cadastro de projetos dentro do banco com sucesso

---
+++ lib/app/modules/home/widgets/header_projects_menu.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import '../../../entities/project_status.dart';
+
+class HeaderProjectsMenu extends SliverPersistentHeaderDelegate{
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Container(
+          padding: EdgeInsets.all(10),
+          color: Colors.white,
+          height: constraints.maxHeight,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                width: constraints.maxWidth * .5,
+                child: DropdownButtonFormField<ProjectStatus>(
+                  items: ProjectStatus.values.map((e) => DropdownMenuItem(child: Text(e.label),value: e,)).toList(),
+                   onChanged: (value){},
+                   decoration: InputDecoration(
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(20) ),
+                    contentPadding: EdgeInsets.all(10),
+                    isCollapsed: true
+                   ),
+                )),
+              SizedBox(
+                width: constraints.maxWidth * .4,
+                child: ElevatedButton.icon(
+                    onPressed: (){
+                      Modular.to.pushNamed('/project/register');
+                    },icon: Icon(Icons.add),label: Text('Novo Projeto'),
+                ))
+            ],
+          ),
+        );
+      }
+    );
+  }
+
+  @override
+  double get maxExtent => 80.0;
+
+  @override
+  double get minExtent => 80.0;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+  
+}
---
+++ lib/app/modules/project/register/controller/project_register_state.dart
@@ -0,0 +1,5 @@
+
+part of 'project_register_controller.dart';
+enum ProjectRegisterStatus {
+  initial,loading,success,failure
+}
